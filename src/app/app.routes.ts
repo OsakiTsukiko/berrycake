1f3a30cc2a2ba1bfa218922167f19b34355c290a
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-05-18 23:52:05 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConnectComponent } from './connect/connect.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'idk', redirectTo: '' },
+    { path: 'connect', component: ConnectComponent },
+  ];
+  
